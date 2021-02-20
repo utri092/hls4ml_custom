84abf901db8378d5a11dbe484e6f43955f2fbe91
utri092 <41176826+utri092@users.noreply.github.com>
2021-02-20 16:54:01 +1300

    Update example_myproject_stream_2.cpp

--- hls4ml/hls4ml_workspace/keras_configs/Zynq_Interface_Code_Files/example_myproject_stream_2.cpp
+++ hls4ml/hls4ml_workspace/keras_configs/Zynq_Interface_Code_Files/example_myproject_stream_2.cpp
@@ -7,6 +7,9 @@
 #include "example_myproject_stream_2.h"
 #include "parameters.h"
 
+
+/** ###################  COPY ########################### **/
+
 void myproject(stream<featuresSdCh> &inStream, stream<featuresSdCh> &outStream)
 {
 
@@ -20,6 +23,7 @@ void myproject(stream<featuresSdCh> &inStream, stream<featuresSdCh> &outStream)
 	// input_layer_array_size will be given in myproject.h
 	const unsigned int row_length = N_INPUT_1_1;
 
+/** ###################  STOP COPY ########################### **/
 
 /** DO NOT COPY. Only to show where to place code in actual main file
 #ifndef __SYNTHESIS__
@@ -41,7 +45,15 @@ void myproject(stream<featuresSdCh> &inStream, stream<featuresSdCh> &outStream)
 #endif
 **/
 
+/** ###################  COPY ########################### **/
+    // ****************************************
+    // NETWORK INSTANTIATION
+    // ****************************************
 
+	/*Replace <dataType> <layer_name> with generated names in header file, e.g.  
+	=> input_t input1   --> for input
+	=> result_t layer11_out [N_LAYER_10]  --> for output
+	*/
 	<hls4ml_generated_dataType> <input_layer_name>[row_length];
 	<hls4ml_generated_dataType> <output_layer_name>[N_LAYER_10];
 
@@ -62,28 +74,76 @@ void myproject(stream<featuresSdCh> &inStream, stream<featuresSdCh> &outStream)
 				
 				inStream.read(valIn);
 				
-				<input_layer_name>[j] = valIn.data;
+				<input_layer_name>[j] = valIn.data; //remember to change <input_layer_name> too
 			}
 
-			/******************  NN functions reside here **************************/
+/** ###################  STOP COPY ########################### **/
 
-			// Write to output Stream after NN functions
-			// Output layer returns an array of size 1 in this example
-			valOut.data = <output_layer_name>[0];
+	/******************  NN functions reside here **************************/
+	 
+	/** ###################  DO NOT COPY ########################### 
 
-			valOut.keep = valIn.keep;
-			valOut.strb = valIn.strb;
-			valOut.user = valIn.user;
-			valOut.id = valIn.id;
-			valOut.dest = valIn.dest;
+		layer2_t layer2_out[N_LAYER_2];
+	    #pragma HLS ARRAY_PARTITION variable=layer2_out complete dim=0
+	    nnet::dense_latency<input_t, layer2_t, config2>(input1, layer2_out, w2, b2);
 
-			valOut.last = valIn.last;
+	    layer3_t layer3_out[N_LAYER_2];
+	    #pragma HLS ARRAY_PARTITION variable=layer3_out complete dim=0
+	    nnet::relu<layer2_t, layer3_t, relu_config3>(layer2_out, layer3_out);
 
-			outStream.write(valOut);
+	    layer4_t layer4_out[N_LAYER_4];
+	    #pragma HLS ARRAY_PARTITION variable=layer4_out complete dim=0
+	    nnet::dense_latency<layer3_t, layer4_t, config4>(layer3_out, layer4_out, w4, b4);
+
+	    layer5_t layer5_out[N_LAYER_4];
+	    #pragma HLS ARRAY_PARTITION variable=layer5_out complete dim=0
+	    nnet::relu<layer4_t, layer5_t, relu_config5>(layer4_out, layer5_out);
+
+	    layer6_t layer6_out[N_LAYER_6];
+	    #pragma HLS ARRAY_PARTITION variable=layer6_out complete dim=0
+	    nnet::dense_latency<layer5_t, layer6_t, config6>(layer5_out, layer6_out, w6, b6);
+
+	    layer7_t layer7_out[N_LAYER_6];
+	    #pragma HLS ARRAY_PARTITION variable=layer7_out complete dim=0
+	    nnet::relu<layer6_t, layer7_t, relu_config7>(layer6_out, layer7_out);
+
+	    layer8_t layer8_out[N_LAYER_8];
+	    #pragma HLS ARRAY_PARTITION variable=layer8_out complete dim=0
+	    nnet::dense_latency<layer7_t, layer8_t, config8>(layer7_out, layer8_out, w8, b8);
+
+	    layer9_t layer9_out[N_LAYER_8];
+	    #pragma HLS ARRAY_PARTITION variable=layer9_out complete dim=0
+	    nnet::relu<layer8_t, layer9_t, relu_config9>(layer8_out, layer9_out);
+
+	    layer10_t layer10_out[N_LAYER_10];
+	    #pragma HLS ARRAY_PARTITION variable=layer10_out complete dim=0
+	    nnet::dense_latency<layer9_t, layer10_t, config10>(layer9_out, layer10_out, w10, b10);
+
+	    nnet::linear<layer10_t, result_t, linear_config11>(layer10_out, layer11_out);
+
+		 ###################  DO NOT COPY ########################### **/
+
+
+/** ###################  COPY ########################### **/
+	/******************  NN functions reside here **************************/
+
+	// Write to output Stream after NN functions
+	// Output layer returns an array of size 1 in this example
+	valOut.data = <output_layer_name>[0];  //remember to change <output_layer_name> too
+
+	valOut.keep = valIn.keep;
+	valOut.strb = valIn.strb;
+	valOut.user = valIn.user;
+	valOut.id = valIn.id;
+	valOut.dest = valIn.dest;
+
+	valOut.last = valIn.last;
+
+	outStream.write(valOut);
 
 	}
 
 }
 
-
+/** ###################  STOP COPY ########################### **/
 
