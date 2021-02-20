0cad5bd68a05f065bf79f8ed1af776bad6063f77
utri092 <41176826+utri092@users.noreply.github.com>
2021-02-20 16:52:59 +1300

    Update example_myproject_stream.cpp

--- hls4ml/hls4ml_workspace/keras_configs/Zynq_Interface_Code_Files/example_myproject_stream.cpp
+++ hls4ml/hls4ml_workspace/keras_configs/Zynq_Interface_Code_Files/example_myproject_stream.cpp
@@ -7,6 +7,7 @@
 #include "parameters.h"
 
 
+/** ###################  COPY ########################### **/
 void myproject(stream<featuresSdCh> &inStream, stream<featuresSdCh> &outStream, unsigned int &max_size)
 {
 
@@ -22,6 +23,8 @@ void myproject(stream<featuresSdCh> &inStream, stream<featuresSdCh> &outStream, 
 
 	// MAX Size of stream. Put in size according to requirements
 	max_size = 0xFFFFFFFF;
+
+/** ###################  STOP COPY ########################### **/
 
 
 /** DO NOT COPY. Only to show where to place code in actual main file
@@ -45,6 +48,11 @@ void myproject(stream<featuresSdCh> &inStream, stream<featuresSdCh> &outStream, 
 **/
 
 
+/** ###################  COPY ########################### **/
+    // ****************************************
+    // NETWORK INSTANTIATION
+    // ****************************************
+
 	/** A nested for loop is needed for iterating through input stream in a 2D manner.
 		-> loop1: Waits on loop2 to read a row of features from input_stream for feeding into input_layer datatype of neural network,
 			  Assigns control signals to output variable
@@ -54,8 +62,12 @@ void myproject(stream<featuresSdCh> &inStream, stream<featuresSdCh> &outStream, 
 			  to hls4ml generated input array. (Check myproject.h for datatype name)	
 	**/
 
+	/*Replace <dataType> <layer_name> with generated names in header file, e.g.  
+	=> input_t input1   --> for input
+	=> result_t layer11_out [N_LAYER_10]  --> for output
+	*/
 	<hls4ml_generated_dataType> <input_layer_name>[row_length];
-	<hls4ml_generated_dataType> <output_layer_name>[LastLayerMacro];
+	<hls4ml_generated_dataType> <output_layer_name>[<LastLayerMacro>];
 
 	// Axi stream data type in myproject.h for reading/writing to input/output streams respectively
 	featuresSdCh valIn, valOut;
@@ -78,17 +90,63 @@ void myproject(stream<featuresSdCh> &inStream, stream<featuresSdCh> &outStream, 
 				
 				inStream.read(valIn);
 				
-				<input_layer_name>[j] = valIn.data;
+				<input_layer_name>[j] = valIn.data;   //remember to change <input_layer_name> too
 			}
 
 
+/** ###################  STOP COPY ########################### **/
 
-			/******************  NN functions reside here **************************/
+	/******************  NN functions reside here **************************/
+	 
+	/** ###################  DO NOT COPY ########################### 
 
-			
+	layer2_t layer2_out[N_LAYER_2];
+    #pragma HLS ARRAY_PARTITION variable=layer2_out complete dim=0
+    nnet::dense_latency<input_t, layer2_t, config2>(input1, layer2_out, w2, b2);
+
+    layer3_t layer3_out[N_LAYER_2];
+    #pragma HLS ARRAY_PARTITION variable=layer3_out complete dim=0
+    nnet::relu<layer2_t, layer3_t, relu_config3>(layer2_out, layer3_out);
+
+    layer4_t layer4_out[N_LAYER_4];
+    #pragma HLS ARRAY_PARTITION variable=layer4_out complete dim=0
+    nnet::dense_latency<layer3_t, layer4_t, config4>(layer3_out, layer4_out, w4, b4);
+
+    layer5_t layer5_out[N_LAYER_4];
+    #pragma HLS ARRAY_PARTITION variable=layer5_out complete dim=0
+    nnet::relu<layer4_t, layer5_t, relu_config5>(layer4_out, layer5_out);
+
+    layer6_t layer6_out[N_LAYER_6];
+    #pragma HLS ARRAY_PARTITION variable=layer6_out complete dim=0
+    nnet::dense_latency<layer5_t, layer6_t, config6>(layer5_out, layer6_out, w6, b6);
+
+    layer7_t layer7_out[N_LAYER_6];
+    #pragma HLS ARRAY_PARTITION variable=layer7_out complete dim=0
+    nnet::relu<layer6_t, layer7_t, relu_config7>(layer6_out, layer7_out);
+
+    layer8_t layer8_out[N_LAYER_8];
+    #pragma HLS ARRAY_PARTITION variable=layer8_out complete dim=0
+    nnet::dense_latency<layer7_t, layer8_t, config8>(layer7_out, layer8_out, w8, b8);
+
+    layer9_t layer9_out[N_LAYER_8];
+    #pragma HLS ARRAY_PARTITION variable=layer9_out complete dim=0
+    nnet::relu<layer8_t, layer9_t, relu_config9>(layer8_out, layer9_out);
+
+    layer10_t layer10_out[N_LAYER_10];
+    #pragma HLS ARRAY_PARTITION variable=layer10_out complete dim=0
+    nnet::dense_latency<layer9_t, layer10_t, config10>(layer9_out, layer10_out, w10, b10);
+
+    nnet::linear<layer10_t, result_t, linear_config11>(layer10_out, layer11_out);
+
+	 ###################  DO NOT COPY ########################### **/
+
+
+/** ###################  COPY ########################### **/
+
+
 			// Write to output Stream after NN functions
 			// Output layer returns an array of size 1 in this example
-			valOut.data = <output_layer_name>[0];
+			valOut.data = <output_layer_name>[0];  //remember to change <output_layer_name> too
 
 			valOut.keep = valIn.keep;
 			valOut.strb = valIn.strb;
@@ -105,4 +163,6 @@ void myproject(stream<featuresSdCh> &inStream, stream<featuresSdCh> &outStream, 
 	}
 
 }
+
+/** ###################  STOP COPY ########################### **/
 
